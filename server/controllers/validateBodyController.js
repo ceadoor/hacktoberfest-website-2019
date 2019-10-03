ee4060e98deda56561db8527f79ea8c6ab3c6751
Abhijith Vijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-10-03 08:09:47 +0530

    Update validateBodyController.js

--- server/controllers/validateBodyController.js
+++ server/controllers/validateBodyController.js
@@ -1,36 +1,21 @@
 const validator = require('express-validator');
 const { validationResult } = require('express-validator');
 
-/* Sample Validation example */
-exports.signUpValidationCriterias = [
-    validator
-        .body('email')
-        .exists()
-        .withMessage('You must provide a valid email address.')
-        .isEmail()
-        .withMessage('Email address you entered is not valid.')
-        .trim()
-        .normalizeEmail(),
-    validator
-        .body('name')
-        .exists()
-        .withMessage('You must provide your name.'),
+exports.fetchHacktoberfestReposValidation = [
     validator
         .body('username')
         .exists()
         .withMessage('You must provide your username.'),    
 ];
 
-exports.signUpValidationBody = (req, res, next) => {
+exports.HacktoberfestReposValidationBody = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         const errorsObj = errors.mapped();
-        const emailError = errorsObj.email && errorsObj.email.msg;
-        const nameError = errorsObj.name && errorsObj.name.msg;
         const usernameError = errorsObj.username && errorsObj.username.msg;
         return res.status(400).json({
             error: {
-                msg: emailError || nameError || usernameError,
+                msg: usernameError,
                 _reported: new Date().getTime(),
             },
         });
