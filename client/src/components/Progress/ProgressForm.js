ef252da8e0063323d5b685ebcfc5502d5a498aec
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-10-03 16:23:14 +0530

    add controlled form using hooks

--- client/src/components/Progress/ProgressForm.js
+++ client/src/components/Progress/ProgressForm.js
@@ -6,43 +6,60 @@ import * as endpoints from '../../api/constants';
 
 const ProgressForm = () => {
     const [validated, setValidated] = useState(false);
+    const [username, setUsername] = useState('');
+
+    const handleChange = event => {
+        const form = event.currentTarget;
+        if (form.checkValidity()) {
+            setValidated(true);
+        }
+        const { value } = event.target;
+        setUsername(value);
+    };
 
     const handleSubmit = event => {
+        event.preventDefault();
+        event.stopPropagation();
+
         const form = event.currentTarget;
         if (form.checkValidity() === false) {
-            event.preventDefault();
-            event.stopPropagation();
+            setValidated(true);
+            return;
         }
-
-        setValidated(true);
-
         // Send POST request
         api({
-            type: 'POST',
+            method: 'POST',
             url: endpoints.GET_PULL_REQUESTS_ENDPOINT,
+            data: {
+                username,
+            },
         });
     };
 
     return (
         <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Form.Row>
-                <Form.Group as={Col} md="7" controlId="validationCustomUsername">
+            <Form.Row className="justify-content-center">
+                <Form.Group as={Col} md="9" lg="8" controlId="GitHub__username">
                     <InputGroup className="mb-3">
-                        <InputGroup.Prepend>
-                            <InputGroup.Text id="basic-addon3">https://github.com/</InputGroup.Text>
+                        <InputGroup.Prepend className="d-sm-block d-none">
+                            <InputGroup.Text id="url__prefix">https://github.com/</InputGroup.Text>
                         </InputGroup.Prepend>
                         <Form.Control
+                            value={username}
+                            onChange={e => {
+                                return handleChange(e);
+                            }}
                             type="text"
                             placeholder="Username"
-                            aria-describedby="inputGroupPrepend"
+                            aria-describedby="url__prepend"
                             required
                         />
-                        <Form.Control.Feedback type="invalid">Please choose a username.</Form.Control.Feedback>
                         <InputGroup.Append>
                             <Button variant="outline-secondary" type="submit">
                                 Check
                             </Button>
                         </InputGroup.Append>
+                        <Form.Control.Feedback type="invalid">Please choose a username.</Form.Control.Feedback>
                     </InputGroup>
                 </Form.Group>
             </Form.Row>
