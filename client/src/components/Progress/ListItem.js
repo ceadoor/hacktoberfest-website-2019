fac3048684122e992eef70bc3589968f66102502
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-10-06 15:51:10 +0530

    app: show icons on progress card

--- client/src/components/Progress/ListItem.js
+++ client/src/components/Progress/ListItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { GitMergeIcon, GitPROpenIcon, GitPRClosedIcon } from '../Icons';
 
-const ListItem = ({ item: { url, repoName, title, number, isPending, createdAt } }) => {
+const ListItem = ({ item: { url, repoName, title, number, isOpen, isMerged, isPending, createdAt } }) => {
     // ToDo: add hacktoberfest label /
     return (
         <a href={url}>
             <div>
-                <div>Icon</div>
+                <div>{(isOpen && <GitPROpenIcon />) || (isMerged && <GitMergeIcon />) || <GitPRClosedIcon />}</div>
                 <div>
                     <span>
                         {repoName}#{number}
