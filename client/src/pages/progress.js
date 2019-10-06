592575c32a2daa6e572452c01fb13d2a672e009e
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-10-07 00:09:55 +0530

    app: auto route to top on other pages

--- client/src/pages/progress.js
+++ client/src/pages/progress.js
@@ -4,10 +4,12 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Progress from '../components/Progress';
 import Layout from '../components/Layout';
+import { ScrollToTop } from '../utils';
 
 const ProgressPage = () => {
     return (
         <>
+            <ScrollToTop />
             <Header />
             <Layout>
                 <Progress />
--- client/src/pages/projects.js
+++ client/src/pages/projects.js
@@ -4,10 +4,12 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Projects from '../components/Projects';
 import Layout from '../components/Layout';
+import { ScrollToTop } from '../utils';
 
 const ProjectsPage = () => {
     return (
         <>
+            <ScrollToTop />
             <Header />
             <Layout>
                 <Projects />
