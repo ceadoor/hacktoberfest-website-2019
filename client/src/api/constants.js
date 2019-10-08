3f0c2e7df3744c92d6563e55bfaec2521f57856d
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2019-10-08 12:52:43 +0530

    app: show remaining seat count list

--- client/src/api/constants.js
+++ client/src/api/constants.js
@@ -1,3 +1,4 @@
 export const GET_PULL_REQUESTS_ENDPOINT = '/getPullRequestsData';
 export const GET_HACKTOBERFEST_REPOS_ENDPOINT = '/getHacktoberfestRepos';
 export const REGISTER_FOR_EVENT_ENDPOINT = '/register';
+export const GET_REMAINING_SEATS_COUNT = '/getRemainingSeatsCount';
--- client/src/components/Register/FormSection.js
+++ client/src/components/Register/FormSection.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import styled from 'styled-components';
 
 import RegisterForm from './RegisterForm';
+import * as endpoints from '../../api/constants';
+import api from '../../api';
 
 const StyledWrapper = styled(Row)`
     margin-top: 50px;
@@ -16,16 +18,41 @@ const StyledWrapper = styled(Row)`
         h2 {
             padding-top: 32px;
             text-align: center;
-            padding-bottom: 1rem;
+        }
+        p {
+            text-align: center;
+            font-size: 16px;
+            padding-bottom: 0.75rem;
         }
     }
 `;
 
 const FormSection = () => {
+    const [remainingSeats, setSeatCount] = useState();
+    const [isErrored, setErrored] = useState(false);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const {
+                    data: { seatsCount },
+                } = await api({
+                    method: 'GET',
+                    url: endpoints.GET_REMAINING_SEATS_COUNT,
+                });
+                setSeatCount(seatsCount);
+            } catch (err) {
+                setErrored(true);
+            }
+        };
+        fetchData();
+    }, []);
+
     return (
         <StyledWrapper>
             <Col md={12} xs={12} className="wrapper">
                 <h2 className="subhead">Register Now</h2>
+                {!remainingSeats && !isErrored && <p>Fetching remaining seats...</p>}
+                {remainingSeats && !isErrored && <p>${remainingSeats} Seats remaining</p>}
                 <RegisterForm />
             </Col>
         </StyledWrapper>
